ba7f6ebe30f78d84fc620571b6547d43862fc7e9
hugosenari <hugosenari@gmail.com>
2022-03-23 22:43:07 -0300

    feat(nimpkgs): remove lock

--- v0_1_2/flake.nix
+++ v0_1_2/flake.nix
@@ -7,19 +7,27 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-nimfuzzy-v0_1_2.flake = false;
-  inputs.src-nimfuzzy-v0_1_2.ref   = "refs/tags/v0.1.2";
-  inputs.src-nimfuzzy-v0_1_2.owner = "genotrance";
-  inputs.src-nimfuzzy-v0_1_2.repo  = "nimfuzzy";
-  inputs.src-nimfuzzy-v0_1_2.type  = "github";
+  inputs.src-nimfuzz-v0_1_2.flake = false;
+  inputs.src-nimfuzz-v0_1_2.ref   = "refs/tags/v0.1.2";
+  inputs.src-nimfuzz-v0_1_2.owner = "genotrance";
+  inputs.src-nimfuzz-v0_1_2.repo  = "nimfuzzy";
+  inputs.src-nimfuzz-v0_1_2.type  = "github";
+  
+  inputs."nimgen".owner = "nim-nix-pkgs";
+  inputs."nimgen".ref   = "master";
+  inputs."nimgen".repo  = "nimgen";
+  inputs."nimgen".dir   = "v0_5_1";
+  inputs."nimgen".type  = "github";
+  inputs."nimgen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimgen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimfuzzy-v0_1_2"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimfuzz-v0_1_2"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-nimfuzzy-v0_1_2";
+    src  = deps."src-nimfuzz-v0_1_2";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
